d03fb4f34ea204ba5168ca7cf2cc56b01400841a
unknown <shaikimam916@gmail.com>
2023-08-01 20:18:34 +0530

    updating

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-var div=React.createElement("div",{},"this is div tag");
+var div=React.createElement("div",{},"this  is a div tag");
 var root2=ReactDOM.createRoot(document.getElementById("root"));
 root2.render(div);
